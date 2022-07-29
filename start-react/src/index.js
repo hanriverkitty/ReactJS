9f498c77d0bebea95ac3ecb3cece6e60fdc42a51
hanriverkitty <danieltls@naver.com>
2022-07-30 08:00:10 +0900

    toDos map

--- start-react/src/index.js
+++ start-react/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import App1 from './App1';
+import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-  <App1 />
+  <App2 />
   //</React.StrictMode>
 );
 
