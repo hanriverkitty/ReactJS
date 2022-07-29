36c55f51f80b5bfb50c5cb57302acdde6ac2eb19
hanriverkitty <danieltls@naver.com>
2022-07-30 00:27:25 +0900

    cleanup

---
+++ start-react/src/App1.js
@@ -0,0 +1,23 @@
+
+import Button from "./Button";
+import styles from "./App.module.css"
+import { useEffect, useState } from "react";
+function Hello() {
+    useEffect(() => {
+        console.log("created :)");
+        return () => console.log("destroyed :(");
+    }, [])
+    return <h1>Hello</h1>
+}
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+        <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    );
+}
+
+export default App;
