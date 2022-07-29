355e3a920f85fac1b069322f4ec8ace23e479649
hanriverkitty <danieltls@naver.com>
2022-07-29 22:02:57 +0900

    effect

--- start-react/src/App.js
+++ start-react/src/App.js
@@ -1,10 +1,19 @@
 
 import Button from "./Button";
 import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  const RunOnce = () => {
+    console.log("run only once.");
+  };
+  useEffect(RunOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
       <Button text={"Continue"} />
     </div>
   );
