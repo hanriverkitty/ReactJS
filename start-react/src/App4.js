7013636f96aded3841d7a87a724512649d24f81a
hanriverkitty <danieltls@naver.com>
2022-07-30 11:13:11 +0900

    router

--- start-react/src/App4.js
+++ start-react/src/App4.js
@@ -1,32 +1,21 @@
-import { useEffect, useState } from "react";
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`)
-        const json = await response.json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getMovies()
-    }, []);
-    return <div>
-        {loading ? <h1>Loading...</h1> : <div>
-            {movies.map((movie) =>
-                <div key={movie.id}>
-                    <img src={movie.medium_cover_image} />
-                    <h2>{movie.title}</h2>
-                    <p>{movie.summary}</p>
-                    <ul>
-                        {movie.genres.map((g) => (
-                            <li key={g}>{g}</li>
-                        ))}
-                    </ul>
-                </div>)}</div>}
-    </div>
+    return <Router>
+        <Switch>
+            <Route path="/movie">
+                <Detail />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </Router>;
 }
 
 export default App;
---
+++ start-react/src/components/Movie.js
@@ -0,0 +1,24 @@
+import ProTypes from "prop-types";
+import { Link } from "react-router-dom";
+function Movie({ coverImg, title, summary, genres }) {
+    return <div>
+        <img src={coverImg} alt={title} />
+        <h2>
+            <Link to="/movie">{title}</Link>
+        </h2>
+        <p>{summary}</p>
+        <ul>
+            {genres.map((g) => (
+                <li key={g}>{g}</li>
+            ))}
+        </ul>
+    </div>;
+}
+Movie.proTypes = {
+    coverImg: ProTypes.string.isRequired,
+    title: ProTypes.string.isRequired,
+    summary: ProTypes.string.isRequired,
+    genres: ProTypes.arrayOf(ProTypes.string).isRequired,
+}
+
+export default Movie;
